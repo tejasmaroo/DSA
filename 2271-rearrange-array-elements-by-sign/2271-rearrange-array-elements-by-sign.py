1a7656e40c0d63c37c2f69f241e34835c9d7cc7e
tejasmaroo <143869588+tejasmaroo@users.noreply.github.com>
2024-10-09 22:37:12 +0530

    Time: 992 ms (79.61%), Space: 44.9 MB (93.81%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
@@ -1,27 +1,15 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        posNums = []
-        negNums = []
-
-        n = len(nums)
-
+        parr = []
+        narr = []
         for i in nums:
-            if i > 0:
-                posNums.append(i)
-            if i < 0:
-                negNums.append(i)
-
-        print("Positive Numbers:", posNums)
-        print("Negative Numbers:", negNums)
-
-        res = []
-
-        for i in range(min(len(posNums), len(negNums))):
-            res.append(posNums[i])   
-            res.append(negNums[i])   
-
-        if len(posNums) > len(negNums):
-            res.extend(posNums[len(negNums):])
-        elif len(negNums) > len(posNums):
-            res.extend(negNums[len(posNums):])
-        return res
+            if i>0:
+                parr.append(i)
+            else:
+                narr.append(i)
+        i,k = 0,0
+        while i<len(parr):
+            nums[k],nums[k+1] = parr[i],narr[i]
+            i += 1
+            k += 2 
+        return nums
