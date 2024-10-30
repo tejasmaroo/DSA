14ad571a9b17fd0ddf33ba3d69fbc0d28da51b5f
tejasmaroo <143869588+tejasmaroo@users.noreply.github.com>
2024-10-30 22:21:40 +0530

    Time: 83 ms (44.98%), Space: 27.2 MB (36.84%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -22,7 +22,6 @@ class Solution:
         elif lenB > lenA:
             for _ in range(lenB - lenA):
                 currB = currB.next
-                
         while currA and currB:
             if currA == currB:
                 return currA
